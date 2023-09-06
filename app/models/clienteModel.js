c325b97e76e89f462c025b9ac47c9ecdf2763889
ericafran <erica.francielle.27@gmail.com>
2023-09-05 21:17:11 -0300

    validação de datas repetidas

--- app/models/clienteModel.js
+++ app/models/clienteModel.js
@@ -48,6 +48,8 @@ module.exports = function () {
 
     this.save = async function (dados, retorno) {
         var nome = dados.nome_feriado;
+        var chave = dados.chave_municipio;
+
         const regexNumber = /\d+/g;
         const regexEspecial = /[^a-zA-Z 0-9]+/g;
 
@@ -60,30 +62,33 @@ module.exports = function () {
 
         if (verificaSeNumero && verificaSeCaractereEspecial == true) {
             var data = dados.data_feriado;
-            var chave = dados.chave_municipio;
+            var resultadoBuscaData = await this.findData(data,chave);
+            console.log(resultadoBuscaData.length);
 
+            if (resultadoBuscaData.length == 0) {
 
-            var queryCadastra = `insert into feriados (nome_feriado,data_feriado,chave_municipio) 
+                var queryCadastra = `insert into feriados (nome_feriado,data_feriado,chave_municipio) 
                  VALUES ('${nome}','${data}',${chave})`;
 
-            const registro = await findName(nome, chave);
+                const registro = await findName(nome, chave);
 
-            console.log(registro.length)
-            if (registro.length == 0) {
-                db.openDb().then(db => {
-                    db.exec(queryCadastra, retorno)
-                    
-                });
+                console.log(registro.length)
+                if (registro.length == 0) {
+                    db.openDb().then(db => {
+                        db.exec(queryCadastra, retorno)
+
+                    });
+
+                } else {
+                    ("Não foi possível cadastrar. Esse feriado, nesta chave já existe ");
+                }
+
             } else {
-                ("Não foi possível cadastrar. Cadastro já existe ");
-
+                (console.log("Já existe um feriado nesta data"))
             }
-
         } else {
-            (console.log("Não foi possível cadastrar"))
+            (console.log("Números ou caracteres inseridos"));
         }
-
-
     }
 
     this.validaCampoNumero = function (validaNumero, res) {
@@ -115,7 +120,7 @@ module.exports = function () {
 
     this.findName = async function (nome, chave_municipio, res) {
 
-        var queryFindName = `select * from feriados where nome_feriado = '${nome}' and chave_municipio = '${chave_municipio}'`;
+        var queryFindName = `select nome_feriado from feriados where nome_feriado = '${nome}' and chave_municipio = '${chave_municipio}'`;
         console.log(queryFindName)
         try {
             return db.openDb().then(db => {
@@ -126,6 +131,19 @@ module.exports = function () {
 
         } catch (e) {
             ('Falha ao procurar nome_feriado');
+        }
+    };
+
+    this.findData = async function (data, chave_municipio, res) {
+        var queryFindData = `select * from feriados where data_feriado = '${data}' and chave_municipio ='${chave_municipio}'`;
+        console.log(queryFindData)
+        try {
+            return db.openDb().then(db => {
+                return db.all(queryFindData)
+                    .then(res => res)
+            });
+        } catch (e) {
+            ("Falha ao procurar data");
         }
     };
 
